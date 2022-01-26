7527ac6e4117e6fd9399ead4b1f344367087fad8
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-01-26 13:35:41 +0200

    Add volume of sphere

---
+++ index.js
@@ -0,0 +1,68 @@
+"Площадь прямоугольного треугольника"
+
+//let a, b
+//a = Number(prompt("сторона а"))
+//b = Number(prompt("сторона b"))
+
+//let s 
+//s = 0.5 * a * b
+
+//console.log('Площадь прямоугольного треугольника = ' + s);
+
+
+"Площадь трапеции"
+
+//let a, b, h
+//a = Number(prompt("сторона а"))
+//b = Number(prompt("сторона b"))
+//h = Number(prompt("перпендикуляр h"))
+
+//let s 
+//s = (a + b) / 2 * h
+
+//console.log('Площадь трапеции = ' + s);
+
+
+"Площадь круга"
+
+//let r
+//r = Number(prompt("Радиус r"))
+
+//let s 
+//s = Math.PI * Math.pow(r, 2)
+
+//console.log('Площадь круга = ' + s);
+
+
+"Площадь параллелограма"
+
+//let a, h
+//a = Number(prompt("cторона а"))
+//h = Number(prompt("Перпендикуляр h"))
+
+//let s 
+//s = a * h
+
+//console.log('Площадь параллелограма = ' + s);
+
+
+"Объем цилиндра"
+
+//let r, h
+//r = Number(prompt("Радиус поверхности r"))
+//h = Number(prompt("Высота h"))
+
+//let v 
+//v = Math.PI * Math.pow(r, 2) * h
+
+//console.log('Объем цилиндра = ' + v);
+
+"Объем сферы"
+
+//let r
+//r = Number(prompt("Радиус шара r"))
+
+//let v 
+//v = 4/3 * Math.PI * Math.pow(r, 3)
+
+//console.log('Объем сферы = ' + v);
